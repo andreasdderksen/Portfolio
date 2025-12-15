835372eda96bea7311d2d0347e1aa1f18173dac4
Andreas Derksen | CM.com <andreas.derksen+cmcom@cm.com>
2025-12-15 11:30:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     <div className="page-transition">
       {/* Navigation */}
       <nav className="nav">
-        <a href="#" className="nav-logo">AD</a>
         <ul className="nav-links">
           <li><a href="#projects">Projects</a></li>
           <li><a href="#about">About</a></li>
